d0d23382c83f552614653aed9170e2e55e90d2ec
Niall Mullins <niall.mullins@rinicom.com>
2021-04-13 16:03:24 +0100

    [FEATURE] Initial commit of deb build
    Some changes to the source and a new script has been added to the
    project which will allow users to build a .deb file for easy
    installation. Newer changes to the project will add the wsdd daemon to
    the same package installation.
    
    In future some additional config will have to be integrated into the
    installation script, for example auto selection of the active network
    interface

--- src/onvif_srvd.cpp
+++ src/onvif_srvd.cpp
@@ -283,7 +283,7 @@ void processing_cfg()
     config_setting_t *setting;
     config_setting_t *profiles;
 
-    if(!config_read_file(&config, "config.cfg"))
+    if(!config_read_file(&config, "/etc/onvif_srvd/config.cfg"))
     {
         fprintf(stderr, "%s:%d - %s\n", config_error_file(&config), config_error_line(&config), config_error_text(&config));
         config_destroy(&config);
