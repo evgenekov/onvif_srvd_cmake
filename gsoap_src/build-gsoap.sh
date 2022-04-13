77e1a1603b09328141cbf360e3889b07531b44f3
Niall Mullins <niall.mullins@rinicom.com>
2022-04-13 14:50:26 +0100

    [PROJECT] Refactored build process and repo file strucure
    Made changes to the cmake build process to achieve the following
    
     * Remove unused files and directories
     * Fix cmake bug where gsoap files were not being generated
     * Remove reliance on make to generage gsoap files
     * Changed gsoap build to use fixed file stored in git-lfs

---
+++ gsoap_src/build-gsoap.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+echo "Building gSOAP"
+GSOAP_INSTALL_DIR=`pwd`/gsoap-2.8
+GSOAP_DIR=$GSOAP_INSTALL_DIR/gsoap
+GSOAP_IMPORT_DIR=$GSOAP_DIR/import
+PATCH_DIR=`pwd`/patches
+
+SOAPCPP2=$GSOAP_DIR/src/soapcpp2
+WSDL2H=$GSOAP_DIR/wsdl/wsdl2h
+GSOAP_CONFIGURE="--disable-c-locale"
+GENERATED_DIR="./generated"
+
+echo $GSOAP_DIR
+echo $GSOAP_INSTALL_DIR
+
+# Check if we already have the generated files needed to build onvif_srvd, if we do then skip the build
+if [[ -f $GENERATED_DIR/DeviceBinding.nsmap &&
+ 	-f $GENERATED_DIR/MediaBinding.nsmap &&
+ 	-f $GENERATED_DIR/onvif.h &&
+ 	-f $GENERATED_DIR/PTZBinding.nsmap &&
+ 	-f $GENERATED_DIR/soapC.cpp &&
+ 	-f $GENERATED_DIR/soapDeviceBindingService.cpp &&
+ 	-f $GENERATED_DIR/soapDeviceBindingService.h &&
+ 	-f $GENERATED_DIR/soapH.h &&
+ 	-f $GENERATED_DIR/soapMediaBindingService.cpp &&
+ 	-f $GENERATED_DIR/soapMediaBindingService.h &&
+ 	-f $GENERATED_DIR/soapPTZBindingService.cpp &&
+ 	-f $GENERATED_DIR/soapPTZBindingService.h &&
+ 	-f $GENERATED_DIR/soapStub.h ]]; then
+    echo "Generated files exist, skipping gsoap build process."
+    exit
+fi
+
+unzip gsoap.zip
+
+echo "Apply Patch"
+cd $GSOAP_DIR && patch -p0 < $PATCH_DIR/stdsoapError.patch
+cd ..
+
+# build
+if [ ! -f $SOAPCPP2 ] || [ ! -f $WSDL2H ]; then \
+        echo "In Build Stage"
+        cd $GSOAP_INSTALL_DIR && \
+        ./configure $GSOAP_CONFIGURE && \
+        ls gsoap/ && \
+        sed -i 's/err = strerror_r(err, soap->msgbuf, sizeof(soap->msgbuf)); \/\* XSI-compliant \*\//\/\/err = strerror_r(err, soap->msgbuf, sizeof(soap->msgbuf)); \/\* XSI-compliant \*\//g' ./gsoap/stdsoap2.c && \
+        make -j8; \
+        cd ..;\
+fi
+
+echo "Generating Files"
+for file in wsdl/*.wsdl
+do
+    WSDL_FILES="${WSDL_FILES} $file"
+done
+
+for file in wsdl/*.xsd
+do
+    WSDL_FILES="${WSDL_FILES} $file"
+done
+
+echo "Create Generated Directory"
+mkdir -p $GENERATED_DIR
+echo "Generate Files"
+$WSDL2H -d -t ./wsdl/typemap.dat -o $GENERATED_DIR/onvif.h $WSDL_FILES
+echo '#import "wsse.h" ' >> $GENERATED_DIR/onvif.h
+echo "First Half"
+$SOAPCPP2 -j -L -x -S -d $GENERATED_DIR -I$GSOAP_DIR:$GSOAP_IMPORT_DIR $GENERATED_DIR/onvif.h
+
+echo "Apply Patch"
+cd $GSOAP_DIR && patch -p0 < $PATCH_DIR/stdsoapLoopFix.patch
+cd ..
+
